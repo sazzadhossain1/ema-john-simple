27fda4ef431f8c5fcc352a3bc2f31ccede89ce06
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-03-24 19:20:08 +0600

    some elements added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="Shop">Shop</a>
+                <a href="Shop">ShOoop</a>
                 <a href="Orders">Orders</a>
                 <a href="inventory">Inventory</a>
                 <a href="about">About</a>
